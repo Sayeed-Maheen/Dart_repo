4a29a23b95aa662c6b925885666c0f37c9ddcb88
sayeed1124 <sayeedhassan1124@gmail.com>
2022-06-06 21:45:22 +0600

    Done

---
+++ for_each.dart
@@ -0,0 +1,6 @@
+void main() {
+  Map info = {"name": "Sayeed", 'age': 25},
+      info2 = {"name": "Hassan", 'age': 26};
+  info.forEach((key, value) => print('${key}: ${value}'));
+  info2.forEach((key, value) => print('${key}: ${value}'));
+}
