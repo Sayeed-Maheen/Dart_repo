35837d70e78e336eddd0f9d1fc8247f9f3d52942
sayeed1124 <sayeedhassan1124@gmail.com>
2022-06-06 21:08:49 +0600

    Done

---
+++ switch_case.dart
@@ -0,0 +1,28 @@
+void main() {
+  var A = 80;
+  switch (A) {
+    case 80:
+      {
+        print("Excellent");
+      }
+      break;
+
+    case 50:
+      {
+        print("Good");
+      }
+      break;
+
+    case 30:
+      {
+        print("Fair");
+      }
+      break;
+
+    default:
+      {
+        print("Fail");
+      }
+      break;
+  }
+}
